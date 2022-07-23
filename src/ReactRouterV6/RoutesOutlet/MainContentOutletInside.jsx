895bc3e5557f82efd9d2b91c29181e62afeaab6a
Anna_Zhyla <anna.s.zhyla@gmail.com>
2022-07-23 20:48:39 +0200

    using NavLink and active classes for it

--- src/ReactRouterV6/RoutesOutlet/MainContentOutletInside.jsx
+++ src/ReactRouterV6/RoutesOutlet/MainContentOutletInside.jsx
@@ -1,13 +1,22 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
 import s from "./RoutesOutlet.module.css";
+
+const setActive = ({ isActive }) => (isActive ? `${s.activeLink}` : ""); // можно передать в className непосредственно функцию (blog), а можно константу с этой функцией (about)
 
 const MainContentOutletInside = () => {
   return (
     <>
       <header className={s.header}>
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
+        <NavLink to="/about" className={setActive}>
+          About
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={({ isActive }) => (isActive ? `${s.activeLink}` : "")}
+        >
+          Blog
+        </NavLink>
       </header>
       <main className={s.main}>
         <div className={s.mainContainer}>
